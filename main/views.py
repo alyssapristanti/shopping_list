9360389895803c0dfcbac3a0f18320a3b01e8077
alyssapristanti <pristantialyssa@gmail.com>
2023-11-15 16:06:55 +0700

    coba tutorial 8

--- main/views.py
+++ main/views.py
@@ -8,10 +8,10 @@ from django.core import serializers
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 from main.forms import ProductForm
 from main.models import Product
-from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required(login_url='/login')
@@ -133,3 +133,21 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
