7f5081428d88862726912dd4926f5212781a64ed
alyssapristanti <pristantialyssa@gmail.com>
2023-11-15 16:30:03 +0700

    tutorial 8

--- main/views.py
+++ main/views.py
@@ -5,13 +5,14 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from main.forms import ProductForm
 from main.models import Product
+import json
 
 
 @login_required(login_url='/login')
